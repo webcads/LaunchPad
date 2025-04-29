384670141e86bd84eee56802f4cd338cfc9a3bb0
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2025-04-29 12:03:47 +0200

    chore: gradle

--- strapi/src/admin/PreviewProxy.tsx
+++ strapi/src/admin/PreviewProxy.tsx
@@ -13,9 +13,12 @@ import { unstable_useDocument as useDocument } from "@strapi/strapi/admin";
 const ExpoPreview = () => {
   const qrCodeSrc = React.useMemo(() => {
     const qrCodeUrl = new URL("https://qr.expo.dev/eas-update");
-    qrCodeUrl.searchParams.append("projectId", "your-project-id");
-    qrCodeUrl.searchParams.append("runtimeVersion", "your-runtime-version");
-    qrCodeUrl.searchParams.append("channel", "your-channel");
+    qrCodeUrl.searchParams.append(
+      "projectId",
+      "4327bdd6-9794-49d7-9b95-6a5198afd339",
+    );
+    qrCodeUrl.searchParams.append("runtimeVersion", "1.0.0");
+    qrCodeUrl.searchParams.append("channel", "production");
     return qrCodeUrl.toString();
   }, []);
 
